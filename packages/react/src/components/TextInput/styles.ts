c4bd3aee12457dd4d2bfe84dfea8893b4fcb4e61
Gabriel Kenji Poli Ozaki <g.kenjiJS@hotmail.com>
2023-04-19 23:12:55 -0300

    :sparkles: feat: Publicando Stories do Componente Visual TextInput

---
+++ packages/react/src/components/TextInput/styles.ts
@@ -0,0 +1,60 @@
+import { styled } from '../../styles'
+
+export const TextInputContainer = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  padding: '$3 $4',
+  border: '2px solid $gray900',
+  borderRadius: '$sm',
+  backgroundColor: '$gray900',
+  boxSizing: 'border-box',
+
+  variants: {
+    size: {
+      sm: {
+        padding: '$2 $3',
+      },
+      md: {
+        padding: '$3 $4',
+      },
+    },
+  },
+
+  '&:has(input:focus)': {
+    borderColor: '$ignite300',
+  },
+
+  '&:has(input:disabled)': {
+    opacity: 0.5,
+    cursor: 'not-allowed',
+  },
+})
+
+export const Prefix = styled('span', {
+  color: '$gray400',
+  fontSize: '$sm',
+  fontWeight: 'regular',
+  fontFamily: '$default',
+})
+
+export const Input = styled('input', {
+  width: '100%',
+  border: 0,
+  background: 'transparent',
+  color: '$white',
+  fontSize: '$sm',
+  fontWeight: 'regular',
+  fontFamily: '$default',
+
+  '&:focus': {
+    outline: 0,
+  },
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+
+  '&::placeholder': {
+    color: '$gray400',
+  },
+})
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -3,5 +3,6 @@ export * from './components/Box'
 export * from './components/Heading'
 export * from './components/Avatar'
 export * from './components/Button'
+export * from './components/TextInput'
 
 export * from './styles'
