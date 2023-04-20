9b726d2f080ac31661a23609a18b52c83f91aabb
Gabriel Kenji Poli Ozaki <g.kenjiJS@hotmail.com>
2023-04-20 00:17:30 -0300

    :sparkles: feat: Publicando Stories do Componente Visual MultiStep

---
+++ packages/react/src/components/MultiStep/styles.ts
@@ -0,0 +1,34 @@
+import { Text } from '../Text'
+
+import { styled } from '../../styles'
+
+export const MultiStepContainer = styled('div', {})
+
+export const Label = styled(Text, {
+  color: '$gray200',
+
+  defaultVariants: {
+    size: 'xs',
+  },
+})
+
+export const Steps = styled('div', {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(var(--steps-size), 1fr)',
+  marginTop: '$1',
+  gap: '$2',
+})
+
+export const Step = styled('div', {
+  height: '$1',
+  borderRadius: '$px',
+  backgroundColor: '$gray600',
+
+  variants: {
+    active: {
+      true: {
+        backgroundColor: '$gray100',
+      },
+    },
+  },
+})
